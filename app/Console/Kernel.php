48cc2482b15c5eb15a738746c5b6f4db3f3ac78a
Garpepi H Aotearoa <garpepi.aotearoa@gmail.com>
2017-05-15 17:08:04 +0700

    Change time scehduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,6 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('Import:AmiIDX --force')
-                 ->dailyAt('18:08');
+                 ->dailyAt('18:09');
     }
 }
